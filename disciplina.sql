76ceafb644cb602ae26e4ddc1c1a2e5e4ef36e59
angoti <angoti@gmail.com>
2020-12-13 07:55:52 -0200

    cadastra nova disciplina

---
+++ disciplina.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `disciplina` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `professor` varchar(45) DEFAULT NULL,
+  `periodo` int(11) DEFAULT NULL,
+  `codigo_sala_classroom` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+
+

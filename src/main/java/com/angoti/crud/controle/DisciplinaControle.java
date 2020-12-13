c5fe335c2324c6a8f0e87e16850789c4fd8e884b
angoti <angoti@gmail.com>
2020-12-13 08:53:08 -0200

    excluir e editar disciplina

--- src/main/java/com/angoti/crud/controle/DisciplinaControle.java
+++ src/main/java/com/angoti/crud/controle/DisciplinaControle.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.angoti.crud.dao.DisciplinaDAO;
 import com.angoti.crud.dominio.Disciplina;
@@ -16,12 +17,12 @@ public class DisciplinaControle {
 
 	@Autowired
 	DisciplinaDAO dao;
-	
+
 	@GetMapping("/disciplinas")
 	public String disciplinasTabela(Model modelo) {
 		List<Disciplina> lista = dao.todos();
-		modelo.addAttribute("lista",lista);
-		return "disciplinas"; 
+		modelo.addAttribute("lista", lista);
+		return "disciplinas";
 	}
 
 	@GetMapping("/disciplina-form")
@@ -29,10 +30,29 @@ public class DisciplinaControle {
 		model.addAttribute("disciplina", new Disciplina());
 		return "disciplina-form";
 	}
-	
+
 	@PostMapping("/disciplina-form")
 	public String processaForm(Disciplina disciplina) {
 		dao.inserir(disciplina);
+		return "redirect:/disciplinas";
+	}
+
+	@GetMapping("/editar-disciplina")
+	public String exibeFormEdit(Model model, @RequestParam(name = "id", required = false) Integer cod){
+		Disciplina disciplina = dao.buscaPorId(cod);
+		model.addAttribute("disciplina", disciplina);
+		return "disciplina-form";
+	}
+
+	@PostMapping("/editar-disciplina")
+	public String processaoFormEdit(Disciplina disciplina) {
+		dao.atualizar(disciplina);
+		return "redirect:/disciplinas";
+	}
+	
+	@GetMapping("/excluir-disciplina")
+	public String excluir(@RequestParam(name = "id", required = true) Integer cod) {
+		dao.excluir(cod);
 		return "redirect:/disciplinas";
 	}
 }

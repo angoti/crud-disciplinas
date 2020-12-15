13bd0b274e6c6ea18555081b8048dedd2939c189
angoti <angoti@gmail.com>
2020-12-15 00:52:32 -0300

    primeira versão - terceira tentativa

--- src/main/java/com/angoti/crud/controle/DisciplinaControle.java
+++ src/main/java/com/angoti/crud/controle/DisciplinaControle.java
@@ -1,5 +1,7 @@
 package com.angoti.crud.controle;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.angoti.crud.dao.DisciplinaDAO;
 import com.angoti.crud.dao.ProfessorDAO;
 import com.angoti.crud.dominio.Disciplina;
-import com.angoti.crud.dominio.Professor;
-
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.mockito.Mockito.after;
-
-import java.util.List;
 
 @Controller
 public class DisciplinaControle {

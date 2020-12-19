6773324f04d4f333e6b6de86503ab6e296e223a4
Edson Angoti Júnior <angoti@gmail.com>
2020-12-19 16:53:53 -0300

    correções

--- src/main/java/com/angoti/crud/controle/DisciplinaControle.java
+++ src/main/java/com/angoti/crud/controle/DisciplinaControle.java
@@ -1,6 +1,7 @@
 package com.angoti.crud.controle;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.angoti.crud.dao.DisciplinaDAO;
 import com.angoti.crud.dominio.Disciplina;
-
-import java.util.List;
 
 @Controller
 public class DisciplinaControle {
